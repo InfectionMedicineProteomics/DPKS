Fixed median normalization to divide by the inter quartile range

dpks/normalization.py
@@ -52,7 +52,7 @@ class MedianNormalization(Normalization):
 
         self.log_transform = log_transform
         self.median = np.ndarray(dtype="f8", shape=(shape[0],))  # type: ignore
-        self.stdev = np.ndarray(dtype="f8", shape=(shape[0],))  # type: ignore
+        self.iqr = np.ndarray(dtype="f8", shape=(shape[0],))  # type: ignore
 
     def fit(self, data: np.ndarray) -> None:
 
@@ -60,8 +60,14 @@ class MedianNormalization(Normalization):
 
             data = np.log(data)
 
-        self.median = np.nanmedian(data, axis=1).reshape((-1, 1))
-        self.stdev = np.nanstd(data, axis=1).reshape((-1, 1))
+        self.median = np.nanpercentile(data, axis=1).reshape((-1, 1))
+
+        q75, q25 = np.percentile(data, [75, 25])
+
+        q75 = q75.reshape((-1, 1))
+        q25 = q25.reshape((-1, 1))
+
+        self.iqr = q75 - q25
 
     def transform(self, data: np.ndarray) -> np.ndarray:
 
@@ -69,4 +75,4 @@ class MedianNormalization(Normalization):
 
             data = np.log(data)
 
-        return (data - self.median) / self.stdev
+        return (data - self.median) / self.iqr