0237326d22966279e87526b0d4dcf7f67a66197d
lars container <lars@container>
2022-03-08 08:42:24 +0100

    added the seaborn dependency

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["Click>=7.0", "numpy", "scipy", "networkx", "pandas"]
+requirements = ["Click>=7.0", "numpy", "scipy", "networkx", "pandas", "seaborn"]
 
 setup_requirements = [
     "pytest-runner",
