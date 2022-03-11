ae44a66fff08f0b407b8c6f4dc67bd6ec514017b
Aaron Scott <aaron.scott@med.lu.se>
2022-03-11 15:13:31 +0100

    Cleaned Quantification file

--- dpks/quantification.py
+++ dpks/quantification.py
@@ -1,39 +1,9 @@
-from abc import ABC, abstractmethod
 from typing import Tuple, List
 
 import networkx as nx  # type: ignore
 import numpy as np  # type: ignore
 
-from enum import Enum
-
 import pandas as pd
-from sklearn.base import TransformerMixin
-
-
-class ProteinQuantificationMethod(Enum):
-    TOP_N_PRECURSORS = 1
-
-
-class ProteinQuantification(ABC):
-    @abstractmethod
-    def init(self, quant_data: np.ndarray, protein_graph: nx.Graph) -> None:
-
-        raise NotImplementedError
-
-    @abstractmethod
-    def quantify(self) -> Tuple[np.ndarray, np.ndarray]:
-
-        raise NotImplementedError
-
-    @abstractmethod
-    def build_protein_group(self, protein_node: str) -> np.ndarray:
-
-        raise NotImplementedError
-
-    @abstractmethod
-    def quantify_protein(self, group_protein: np.ndarray) -> np.ndarray:
-
-        raise NotImplementedError
 
 
 class TopN:
