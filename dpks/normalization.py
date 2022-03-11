fe9df3cba9528bff62f4c06e9115d1f8c7fd9b32
Aaron Scott <aaron.scott@med.lu.se>
2022-03-11 13:19:40 +0100

    Added TicNormalization

--- dpks/normalization.py
+++ dpks/normalization.py
@@ -4,6 +4,8 @@ import numpy as np  # type: ignore
 
 from enum import Enum
 
+from sklearn.base import TransformerMixin
+
 
 class NormalizationMethod(Enum):
     MEAN = 1
@@ -13,13 +15,30 @@ class NormalizationMethod(Enum):
 class Normalization(ABC):
     @abstractmethod
     def fit(self, data: np.ndarray) -> None:
-
         raise NotImplementedError
 
     @abstractmethod
     def transform(self, data: np.ndarray) -> np.ndarray:
-
         raise NotImplementedError
+
+
+class TicNormalization(TransformerMixin):
+
+    def __init__(self):
+
+        pass
+
+    def fit_transform(self, X, y=None, **fit_params):
+
+        sample_sums = np.nansum(X, axis=0)
+
+        median_signal = np.nanmedian(sample_sums)
+
+        normalized_signal = (X / sample_sums[None, :]) * median_signal
+
+        normalized_signal = np.log2(normalized_signal)
+
+        return normalized_signal
 
 
 class MeanNormalization(Normalization):
@@ -32,7 +51,6 @@ class MeanNormalization(Normalization):
     def fit(self, data: np.ndarray) -> None:
 
         if self.log_transform:
-
             data = np.log(data)
 
         self.mean = np.nanmean(data, axis=1).reshape((-1, 1))
@@ -41,7 +59,6 @@ class MeanNormalization(Normalization):
     def transform(self, data: np.ndarray) -> np.ndarray:
 
         if self.log_transform:
-
             data = np.log(data)
 
         return (data - self.mean) / self.stdev
@@ -57,7 +74,6 @@ class MedianNormalization(Normalization):
     def fit(self, data: np.ndarray) -> None:
 
         if self.log_transform:
-
             data = np.log(data)
 
         self.median = np.nanmedian(data, axis=1).reshape((-1, 1))
@@ -66,7 +82,6 @@ class MedianNormalization(Normalization):
     def transform(self, data: np.ndarray) -> np.ndarray:
 
         if self.log_transform:
-
             data = np.log(data)
 
         return (data - self.median) / self.stdev
