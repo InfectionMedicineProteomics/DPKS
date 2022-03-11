c9a730b9c8922b56c251631ab06ac884d9e7851e
Aaron Scott <aaron.scott@med.lu.se>
2022-03-11 15:12:54 +0100

    Updated QuantMatrix API to use TopN protein quantification through the quantify() function

--- dpks/quant_matrix.py
+++ dpks/quant_matrix.py
@@ -184,56 +184,31 @@ class QuantMatrix:
 
     def quantify(
         self,
-        method: ProteinQuantificationMethod,
-        top_n: int = 1,
-        protein_grouping: str = "",
+        method: str,
+        resolve_protein_groups: bool = False,
+        **kwargs
     ):
 
-        if method.value == ProteinQuantificationMethod.TOP_N_PRECURSORS.value:
+        if resolve_protein_groups:
 
-            quantification = TopNPrecursors(
-                top_n=top_n, protein_grouping=protein_grouping
+            pass
+
+        if method == "top_n":
+
+            quantifications = TopN(top_n=kwargs['top_n']).quantify(
+                self
             )
 
-        quantification.init(self.matrix, self.quant_graph)
+            design_matrix = self.quantitative_data.var
 
-        quantified_protein_ids, quantified_protein_data = quantification.quantify()
+            protein_quantifications = QuantMatrix(
+                quantifications,
+                design_matrix_file=design_matrix
+            )
 
-        return self._copy(
-            num_samples=self.num_samples,
-            num_quant_records=self.num_quant_records,
-            quant_type=self.quant_type,
-            design_matrix=self.design_matrix,
-            matrix=quantified_protein_data,
-            quant_graph=self.quant_graph,
-            quant_record_index=quantified_protein_ids,
-        )
+            return protein_quantifications
 
-    def _copy(
-        self,
-        num_samples: int = 0,
-        num_quant_records: int = 0,
-        quant_type: str = "",
-        design_matrix: list = None,
-        matrix: np.ndarray = None,
-        quant_graph: nx.Graph = None,
-        quant_record_index: np.ndarray = None,
-    ):
-
-        cloned = self.__class__
-        obj = cloned(
-            num_samples=num_samples,
-            num_quant_records=num_quant_records,
-            quant_type=quant_type,
-            design_matrix=design_matrix,
-            matrix=matrix,
-            quant_record_index=quant_record_index,
-            quant_graph=quant_graph,
-        )
-
-        return obj
-
-    def differentiate_expression(self):
+    def test_differential_expression(self):
 
         pass
 
@@ -253,138 +228,3 @@ class QuantMatrix:
     def write(self, file_path: str = ""):
 
         pass
-
-    def as_dataframe(self, level: str = ""):
-
-        records = []
-
-        if level == "protein":
-
-            num_proteins: int = self.quant_record_index.shape[0]
-
-            for protein_index in range(num_proteins):
-
-                protein_id: str = self.quant_record_index[protein_index]
-
-                record: dict = {
-                    "Protein": protein_id,
-                }
-
-                for sample_index, sample_data in enumerate(self.design_matrix):
-
-                    record[sample_data["name"]] = self.matrix[
-                        protein_index, sample_index
-                    ]
-
-                records.append(record)
-
-        if level == "precursor":
-
-            pass
-
-        if level == "peptide":
-
-            pass
-
-        if level == "fragment":
-
-            pass
-
-        return pd.DataFrame(records)
-
-    @classmethod
-    def from_csv(
-        cls, file_path: str = "", design_matrix_path: str = "", quant_type: str = ""
-    ) -> QuantMatrix:
-
-        assert quant_type in ["fragment", "precursor", "peptide", "protein"]
-
-        design_matrix = list()
-
-        with open(design_matrix_path, "r") as design_matrix_path_h:
-
-            csv_reader = DictReader(design_matrix_path_h)
-
-            for record in csv_reader:
-
-                design_matrix_record = {"name": record["Sample"]}
-
-                if "Group" in record:
-
-                    design_matrix_record["group"] = record["Group"]
-
-                if "Batch" in record:
-
-                    design_matrix_record["batch"] = record["Batch"]
-
-                design_matrix.append(design_matrix_record)
-
-        with open(file_path, "r") as quant_file_h:
-
-            csv_reader = DictReader(quant_file_h)
-
-            records = list(csv_reader)
-
-            num_quant_records = len(records)
-
-            quant_matrix = QuantMatrix(
-                design_matrix=design_matrix,
-                num_samples=len(design_matrix),
-                num_quant_records=num_quant_records,
-                quant_type=quant_type,
-            )
-
-            for index, record in enumerate(records):
-
-                if quant_type == "precursor":
-
-                    precursor_id = f"{record['PeptideSequence']}_{record['Charge']}"
-
-                    quant_matrix.quant_record_index[index] = precursor_id
-
-                    precursor = Precursor(
-                        peptide_sequence=record["PeptideSequence"],
-                        charge=record["Charge"],  # type: ignore
-                        decoy=record["Decoy"],  # type: ignore
-                        retention_time=record["RetentionTime"],  # type: ignore
-                        index=index,
-                    )
-
-                    quant_matrix.quant_graph.add_nodes_from(
-                        [precursor_id], data=precursor, bipartite="precursor"
-                    )
-
-                    protein_accessions = record["Protein"].split(";")
-
-                    for protein_accession in protein_accessions:
-
-                        protein = Protein(accession=protein_accession)
-
-                        if protein_accession not in quant_matrix.quant_graph:
-
-                            quant_matrix.quant_graph.add_nodes_from(
-                                [protein_accession], data=protein, bipartite="protein"
-                            )
-
-                        quant_matrix.quant_graph.add_edges_from(
-                            [(protein_accession, precursor_id)]
-                        )
-
-                    for sample_index, sample_info in enumerate(design_matrix):
-
-                        sample_name = sample_info["name"]
-
-                        intensity = record[sample_name]
-
-                        quant_matrix.matrix[index, sample_index] = intensity
-
-        return quant_matrix
-
-
-def create_quant_matrix(
-    file_path: str = "", design_matrix: str = "", quant_type: str = ""
-) -> QuantMatrix:
-
-    print("Parsing Quant Matrix and building data structure.")
-
-    return QuantMatrix.from_csv(file_path, design_matrix, quant_type)
