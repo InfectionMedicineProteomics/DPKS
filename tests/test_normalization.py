d43498d73623bc939c38b9700a22f4bf81b5c174
lars container <lars@container>
2022-03-12 08:04:38 +0100

    adapted to the new interface

--- tests/test_normalization.py
+++ tests/test_normalization.py
@@ -2,18 +2,15 @@
 """Tests for the normalization module"""
 
 import pytest
-import dpks.quant_matrix as quant
-from dpks.normalization import NormalizationMethod
+from dpks.quant_matrix import QuantMatrix
 
 
 @pytest.mark.normalization()
 def test_normalization_mean(paths, quant_matrix):
     """normalization mean"""
 
-    assert type(quant_matrix) == quant.QuantMatrix
+    assert type(quant_matrix) == QuantMatrix
 
-    normalized_peptide_quantities = quant_matrix.normalize(
-        method=NormalizationMethod.MEAN
-    )
+    normalized_peptide_quantities = quant_matrix.normalize(method="mean")
 
-    assert type(normalized_peptide_quantities) == quant.QuantMatrix
+    assert type(normalized_peptide_quantities) == QuantMatrix
