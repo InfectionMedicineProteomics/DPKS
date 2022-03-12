6b69ab0db7ccb65e9c3a90d7a2b90e830cfb6e7f
lars container <lars@container>
2022-03-12 06:51:24 +0100

    added anndata and sklearn dependencies

--- setup.py
+++ setup.py
@@ -10,7 +10,16 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["Click>=7.0", "numpy", "scipy", "networkx", "pandas", "seaborn"]
+requirements = [
+    "Click>=7.0",
+    "numpy",
+    "scipy",
+    "networkx",
+    "pandas",
+    "seaborn",
+    "anndata",
+    "sklearn",
+]
 
 setup_requirements = [
     "pytest-runner",
