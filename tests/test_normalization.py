8f8dd1da25a0173908db55f933ac588036ecaf4b
lars container <lars@container>
2022-03-24 10:21:56 +0100

    added tests for tic, mean, and sliding rt normalization

--- tests/test_normalization.py
+++ tests/test_normalization.py
@@ -6,11 +6,52 @@ from dpks.quant_matrix import QuantMatrix
 
 
 @pytest.mark.normalization()
-def test_normalization_mean(paths, quant_matrix):
+def test_normalization_mean(quant_matrix):
     """normalization mean"""
 
-    assert type(quant_matrix) == QuantMatrix
+    assert isinstance(quant_matrix, QuantMatrix)
 
     normalized_peptide_quantities = quant_matrix.normalize(method="mean")
 
-    assert type(normalized_peptide_quantities) == QuantMatrix
+    assert isinstance(normalized_peptide_quantities, QuantMatrix)
+
+
+@pytest.mark.normalization()
+def test_normalization_tic(quant_matrix):
+    """normalization tic"""
+
+    assert isinstance(quant_matrix, QuantMatrix)
+
+    normalized_peptide_quantities = quant_matrix.normalize(method="tic")
+
+    assert isinstance(normalized_peptide_quantities, QuantMatrix)
+
+
+@pytest.mark.normalization()
+def test_normalization_median(quant_matrix):
+    """normalization median"""
+
+    assert isinstance(quant_matrix, QuantMatrix)
+
+    normalized_peptide_quantities = quant_matrix.normalize(method="median")
+
+    assert isinstance(normalized_peptide_quantities, QuantMatrix)
+
+
+@pytest.mark.normalization()
+def test_normalization_rt_median(quant_matrix):
+    """normalization median"""
+
+    assert isinstance(quant_matrix, QuantMatrix)
+
+    normalized_peptide_quantities = quant_matrix.normalize(
+        method="mean",
+        log_transform=True,
+        use_rt_sliding_window_filter=True,
+        minimum_data_points=100,
+        stride=5,
+        use_overlapping_windows=True,
+        rt_unit="seconds",
+    )
+
+    assert isinstance(normalized_peptide_quantities, QuantMatrix)
