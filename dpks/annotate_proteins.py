9610f9fe878980c7a4505fda9fcfa38650b629ae
Aaron Scott <aaron.scott@med.lu.se>
2023-01-25 13:33:25 +0100

    Fixed parallel loop in quanitfying proteins, numba typed lists are not thread safe, so indicating the size and datatypes to hold data and then reformatting the returned results seems to have worked

--- dpks/annotate_proteins.py
+++ dpks/annotate_proteins.py
@@ -1,18 +1,22 @@
+from typing import List
+
 from Bio import SeqIO
 import gzip
+
 
 def parse_fasta(fasta):
     fasta_dict = {}
     for record in SeqIO.parse(fasta, "fasta"):
         id = record.id
-        accession = id.split('|')[1]
-        name = id.split('|')[2]
+        accession = id.split("|")[1]
+        name = id.split("|")[2]
         fasta_dict[accession] = name
-    return fasta_dict 
+    return fasta_dict
+
 
 def fasta_to_dict(fasta_path: str) -> dict:
     fasta_dict = {}
-    if fasta_path.endswith('gz'):
+    if fasta_path.endswith("gz"):
         with gzip.open(fasta_path, "rt") as handle:
             fasta_dict = parse_fasta(handle)
     else:
@@ -20,11 +24,10 @@ def fasta_to_dict(fasta_path: str) -> dict:
     return fasta_dict
 
 
-
-def get_protein_labels(accessions : list[str], fasta_path : str) -> list[str]:
+def get_protein_labels(accessions: List[str], fasta_path: str) -> List[str]:
     """Maps UniProt ID (accession) to more common names. P69905 --> HBA_HUMAN
     Args:
-        accessions list[str]: Input protein list 
+        accessions list[str]: Input protein list
         fasta_path str: fasta file path used for mapping
     Returns:
         protein_labels list[str]: More common protein names. If accession not in human_proteome.gz, use accession.
@@ -32,15 +35,10 @@ def get_protein_labels(accessions : list[str], fasta_path : str) -> list[str]:
     fasta_dict = fasta_to_dict(fasta_path)
     protein_labels = []
     for accession in accessions:
-        try: 
+        try:
             protein_label = fasta_dict[accession]
         except KeyError:
             # If accession not in fasta file, put accession instead of protein label
             protein_label = accession
         protein_labels.append(protein_label)
     return protein_labels
-
-
-
-    
-    
