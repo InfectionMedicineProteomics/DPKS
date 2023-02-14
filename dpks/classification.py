a0bc084bd8ee91f78a1ac4c1516a04cdb327b324
ErikHartman <erik.hartman@hotmail.com>
2023-02-14 16:31:02 +0100

    SHAP is working and added recursive feature elimination which seems to be working

--- dpks/classification.py
+++ dpks/classification.py
@@ -1,9 +1,10 @@
 from typing import TYPE_CHECKING, Any
 import xgboost
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import cross_val_score
 import numpy as np
 import shap
+from sklearn.feature_selection import RFECV
 
 
 if TYPE_CHECKING:
@@ -13,14 +14,16 @@ else:
 
 
 class Classifier:
+    X: np.array
+    Y: np.array
     shap_values: list
-    quantitative_data: QuantMatrix
+    mean_importance: list
 
-    def __init__(self, classifier, quantitative_data: QuantMatrix):
-        self.X, self.Y = self._generate_data_matrices(quantitative_data)
+    def __init__(self, classifier, quantitative_data: QuantMatrix, scale: bool = True):
+        self.X, self.Y = self._generate_data_matrices(quantitative_data, scale)
         if isinstance(classifier, str):
             if classifier == "xgboost":
-                self.clf = xgboost.XGBClassifier(max_depth=15)
+                self.clf = xgboost.XGBClassifier(max_depth=30)
         else:
             fit_method = getattr(classifier, "fit", None)
             predict_method = getattr(classifier, "predict", None)
@@ -32,8 +35,7 @@ class Classifier:
                 )
 
     def fit(self):
-        self.clf.fit(self.X, self.Y)
-        return self.clf
+        return self.clf.fit(self.X, self.Y)
 
     def cross_validation(self, k_folds: int = 5):
         scores = cross_val_score(self.clf, self.X, self.Y, cv=k_folds)
@@ -42,23 +44,55 @@ class Classifier:
             % (scores.mean(), scores.std())
         )
 
-    def interpret(self, algorithm: str = "auto", pass_predict_to_shap: bool = False):
+    def recursive_feature_elimination(
+        self,
+        k_folds: int = 5,
+        scoring: str = "accuracy",
+        min_features_to_select: int = 10,
+        step: int = 10,
+    ):
+        selector = RFECV(
+            self.clf,
+            step=step,
+            min_features_to_select=min_features_to_select,
+            scoring=scoring,
+            cv=k_folds,
+        )
+
+        selector = selector.fit(self.X, self.Y)
+        print(f"Number of selected features: {selector.n_features_}")
+        print(selector.support_)
+        print(selector.ranking_)
+
+    def interpret(self, quantitative_data, algorithm: str = "auto") -> QuantMatrix:
         clf = self.fit()
 
         if algorithm == "permutation":
             explainer = shap.Explainer(clf.predict, self.X, algorithm=algorithm)
             self.shap_values = explainer(self.X, max_evals=2 * self.X.shape[1] + 1)
         elif algorithm == "tree":
+            explainer = shap.Explainer(clf, algorithm=algorithm)
+            self.shap_values = explainer.shap_values(self.X)
+        elif algorithm == "auto":
             explainer = shap.Explainer(clf, self.X, algorithm=algorithm)
             self.shap_values = explainer.shap_values(self.X)
 
-    def _generate_data_matrices(self, quantitative_data: QuantMatrix) -> tuple:
+        self.mean_importance = np.mean(abs(self.shap_values), axis=0)
+        quantitative_data.row_annotations[f"SHAP"] = self.mean_importance
+
+        return quantitative_data
+
+    def _generate_data_matrices(
+        self, quantitative_data: QuantMatrix, scale: bool
+    ) -> tuple:
         le = LabelEncoder()
         Y = le.fit_transform(quantitative_data.quantitative_data.var["group"].values)
         X = quantitative_data.quantitative_data.X.copy().transpose()
         X = np.nan_to_num(X, copy=True, nan=0.0)
+        if scale:
+            X = StandardScaler().fit_transform(X)
         return X, Y
 
     @property
-    def feature_importance_(self):
-        return self.shap_values
+    def feature_importances_(self):
+        return self.mean_importance
