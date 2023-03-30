39f259b4265fe58832c2c63076b9f6dff14184e3
ErikHartman <erik.hartman@hotmail.com>
2023-03-30 15:24:26 +0200

    add parameter search to classifier

--- dpks/classification.py
+++ dpks/classification.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING, Any
 import xgboost
-
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
 import numpy as np
 import shap
@@ -39,6 +40,33 @@ class Classifier(BaseEstimator, ClassifierMixin):
                     "The classifier does not have a fit and/or predict method"
                 )
         self.shap_algorithm = shap_algorithm
+
+    def get_best_estimator(
+        self,
+        X,
+        y,
+        param_grid: dict,
+        folds: int = 3,
+        random_state: int = None,
+        n_iter: int = 30,
+        n_jobs: int = 4,
+        scoring: str = "accuracy",
+    ):
+        skf = StratifiedKFold(n_splits=folds, shuffle=True, random_state=random_state)
+        random_search = RandomizedSearchCV(
+            self.classifier,
+            param_distributions=param_grid,
+            n_iter=n_iter,
+            n_jobs=n_jobs,
+            cv=skf.split(X, y),
+            verbose=0,
+            scoring=scoring,
+            return_train_score=True,
+        )
+        random_search.fit(X, y)
+        self.best_params = random_search.best_params_
+        self.classifier = random_search.best_estimator_
+        return self.classifier
 
     def fit(self, X, y):
         self.classifier.fit(X, y)
