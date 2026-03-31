97aebd8a70474dba352b93fe26a0bf730ee140cf
fi7855ar <filip.arman@med.lu.se>
2026-03-31 17:34:16 +0200

    Add tests

--- tests/test_differential_testing.py
+++ tests/test_differential_testing.py
@@ -33,3 +33,32 @@ def test_differential_testing(paths):
     )
 
     assert isinstance(compared_data, QuantMatrix)
+
+
+def test_differential_testing_paired(paths):
+    """test the differential_testing object"""
+
+    quant_matrix = QuantMatrix(
+        quantification_file=str(paths["de_matrix_path"]),
+        design_matrix_file=str(paths["de_design_matrix_paired_path"]),
+    )
+    compared_data = (
+        quant_matrix.filter()
+        .normalize(
+            method="mean",
+            #                log_transform=True,
+            #                use_rt_sliding_window_filter=True,
+            #                window_length=100,
+            #                stride=1
+        )
+        .quantify(method="top_n", top_n=1)
+        .compare(
+            method="ttest_paired",
+            comparisons=(4,6),
+            min_samples_per_group=4,
+            level="protein",
+            multiple_testing_correction_method="fdr_tsbh",
+        )
+    )
+
+    assert isinstance(compared_data, QuantMatrix)
