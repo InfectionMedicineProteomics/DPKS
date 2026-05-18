c38cb62e88ad71281663d5735f0bc48dcab78aef
Aaron Scott <aaron.scott@med.lu.se>
2026-05-18 14:37:36 +0200

    Removed unused imports from QuantMatrix

--- dpks/quant_matrix.py
+++ dpks/quant_matrix.py
@@ -22,7 +22,6 @@ from sklearn.pipeline import Pipeline
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.model_selection import cross_val_score, StratifiedKFold, cross_val_predict
 from sklearn.preprocessing import StandardScaler, LabelEncoder
-from statsmodels.genmod.qif import QIFAutoregressive
 
 from dpks.annotation import get_protein_labels, get_genes_from_proteins
 from dpks.clustering import FeatureClustering
