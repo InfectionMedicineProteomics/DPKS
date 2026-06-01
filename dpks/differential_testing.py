590c1d72295a017450e560c2a3e2c049c3508026
Aaron Scott <aaron.scott@med.lu.se>
2026-06-01 10:23:11 +0200

    Did not need to recalculate the number of parameters (q) each iteration, it never changes

--- dpks/differential_testing.py
+++ dpks/differential_testing.py
@@ -76,7 +76,6 @@ class FastOLS:
             X_subset = X_subset[X_mask]
 
             n_subset = X_subset.shape[0]
-            q_subset = Z_subset.shape[1]
 
             Q, R = np.linalg.qr(Z_subset)
             QtX = Q.T @ X_subset
@@ -84,14 +83,14 @@ class FastOLS:
             X_fitted = Z_subset @ B_hat
 
             _residuals = X_subset - X_fitted
-            sigma2 = np.sum(_residuals ** 2, axis=0) / (n_subset - q_subset)
+            sigma2 = np.sum(_residuals ** 2, axis=0) / (n_subset - q)
 
             r_inv = np.linalg.inv(R)
             diag_inv = np.sum(r_inv ** 2, axis=1)
             standard_error = np.sqrt(np.outer(diag_inv, sigma2))
             t_statistics = B_hat / standard_error
 
-            degrees_of_freedom = n_subset - q_subset
+            degrees_of_freedom = n_subset - q
             _pvalues = 2 * stats.t.sf(np.abs(t_statistics), df=degrees_of_freedom)
 
             params[:, cols_] = B_hat
